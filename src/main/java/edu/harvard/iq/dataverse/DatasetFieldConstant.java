75352f507fafe4b51e5ab4308216ae0b1fe1b704
Philip Durbin <philip_durbin@harvard.edu>
2014-07-24 14:21:47 -0400

    distributorContact became datasetContact in #754

--- src/main/java/edu/harvard/iq/dataverse/DatasetFieldConstant.java
+++ src/main/java/edu/harvard/iq/dataverse/DatasetFieldConstant.java
@@ -26,7 +26,7 @@ public class DatasetFieldConstant implements java.io.Serializable  {
     public final static String software = "software";
     public final static String grantNumber = "grantNumber";
     public final static String distributor = "distributor";
-    public final static String distributorContact = "distributorContact";
+    public final static String datasetContact = "datasetContact";
     public final static String series = "series";
     public final static String datasetVersion = "datasetVersion";
     
@@ -564,8 +564,8 @@ public class DatasetFieldConstant implements java.io.Serializable  {
         return author;
     }
 
-    public String getDistributorContact() {
-        return distributorContact;
+    public String getDatasetContact() {
+        return datasetContact;
     }
 
     public String getDescription() {
