1a3564032d42b533c5ac76ab800d6fa14d293dd7
bencomp <ben@companjen.name>
2015-09-25 02:08:42 +0200

    Add functioning test stubs for DatasetPage

---
+++ src/test/java/edu/harvard/iq/dataverse/DatasetPageTest.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.harvard.iq.dataverse;
+
+import edu.harvard.iq.dataverse.authorization.Permission;
+import edu.harvard.iq.dataverse.search.FacetCategory;
+import edu.harvard.iq.dataverse.search.FileView;
+import edu.harvard.iq.dataverse.search.SolrSearchResult;
+import java.util.List;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.ValueChangeEvent;
+import javax.faces.model.SelectItem;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.primefaces.event.FileUploadEvent;
+
+/**
+ *
+ * @author ben
+ */
+public class DatasetPageTest {
+    
+    public DatasetPageTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * Test of canDownloadFile method, of class DatasetPage.
+     */
+    @Test
+    public void testCanDownloadFile() {
+        System.out.println("canDownloadFile");
+        FileMetadata fileMetadata = null;
+        DatasetPage instance = new DatasetPage();
+        boolean result = instance.canDownloadFile(fileMetadata);
+        assertFalse(result);
+    }
+
+    /**
+     * Test of isShapefileType method, of class DatasetPage.
+     */
+    @Test
+    public void testIsShapefileType() {
+        System.out.println("isShapefileType");
+        FileMetadata fm = null;
+        DatasetPage instance = new DatasetPage();
+        boolean result = instance.isShapefileType(fm);
+        assertFalse(result);
+    }
+
+    /**
+     * Test of hasMapLayerMetadata method, of class DatasetPage.
+     */
+    @Test
+    public void testHasMapLayerMetadata() {
+        System.out.println("hasMapLayerMetadata");
+        FileMetadata fm = null;
+        DatasetPage instance = new DatasetPage();
+        boolean result = instance.hasMapLayerMetadata(fm);
+        assertFalse(result);
+    }
+
+    /**
+     * Test of canSeeMapButtonReminderToPublish method, of class DatasetPage.
+     */
+    @Test
+    public void testCanSeeMapButtonReminderToPublish() {
+        System.out.println("canSeeMapButtonReminderToPublish");
+        FileMetadata fm = null;
+        DatasetPage instance = new DatasetPage();
+        boolean result = instance.canSeeMapButtonReminderToPublish(fm);
+        assertFalse(result);
+    }
+
+    /**
+     * Test of canUserSeeMapDataButton method, of class DatasetPage.
+     */
+    @Test
+    public void testCanUserSeeMapDataButton() {
+        System.out.println("canUserSeeMapDataButton");
+        FileMetadata fm = null;
+        DatasetPage instance = new DatasetPage();
+        boolean result = instance.canUserSeeMapDataButton(fm);
+        assertFalse(result);
+    }
+
+    /**
+     * Test of canUserSeeExploreWorldMapButton method, of class DatasetPage.
+     */
+    @Test
+    public void testCanUserSeeExploreWorldMapButton() {
+        System.out.println("canUserSeeExploreWorldMapButton");
+        FileMetadata fm = null;
+        DatasetPage instance = new DatasetPage();
+        boolean result = instance.canUserSeeExploreWorldMapButton(fm);
+        assertFalse(result);
+    }
+    
+}
