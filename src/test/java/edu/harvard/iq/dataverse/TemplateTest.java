41f5303a87abf8f7bf7fafd9456ab7779809ec4a
bencomp <ben@companjen.name>
2022-10-14 00:14:35 +0200

    Add generated TemplateTest

---
+++ src/test/java/edu/harvard/iq/dataverse/TemplateTest.java
@@ -0,0 +1,165 @@
+package edu.harvard.iq.dataverse;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TemplateTest {
+  @Test
+  void testCloneNewTemplate() {
+
+  }
+
+  @Test
+  void testCopyDatasetFields() {
+
+  }
+
+  @Test
+  void testEquals() {
+
+  }
+
+  @Test
+  void testGetCreateDate() {
+
+  }
+
+  @Test
+  void testGetCreateTime() {
+
+  }
+
+  @Test
+  void testGetDatasetFields() {
+
+  }
+
+  @Test
+  void testGetDataverse() {
+
+  }
+
+  @Test
+  void testGetDataversesHasAsDefault() {
+
+  }
+
+  @Test
+  void testGetFlatDatasetFields() {
+    Template template = new Template();
+    assertNotNull(template.getFlatDatasetFields());
+    assertTrue(template.getFlatDatasetFields().isEmpty());
+  }
+
+  @Test
+  void testGetId() {
+
+  }
+
+  @Test
+  void testGetInstructionsFor() {
+
+  }
+
+  @Test
+  void testGetInstructionsMap() {
+
+  }
+
+  @Test
+  void testGetMetadataBlocksForEdit() {
+
+  }
+
+  @Test
+  void testGetMetadataBlocksForView() {
+
+  }
+
+  @Test
+  void testGetName() {
+
+  }
+
+  @Test
+  void testGetTermsOfUseAndAccess() {
+
+  }
+
+  @Test
+  void testGetUsageCount() {
+
+  }
+
+  @Test
+  void testHashCode() {
+
+  }
+
+  @Test
+  void testIsIsDefaultForDataverse() {
+
+  }
+
+  @Test
+  void testSetCreateTime() {
+
+  }
+
+  @Test
+  void testSetDatasetFields() {
+
+  }
+
+  @Test
+  void testSetDataverse() {
+
+  }
+
+  @Test
+  void testSetDataversesHasAsDefault() {
+
+  }
+
+  @Test
+  void testSetIsDefaultForDataverse() {
+
+  }
+
+  @Test
+  void testSetMetadataBlocksForEdit() {
+
+  }
+
+  @Test
+  void testSetMetadataBlocksForView() {
+
+  }
+
+  @Test
+  void testSetMetadataValueBlocks() {
+
+  }
+
+  @Test
+  void testSetName() {
+
+  }
+
+  @Test
+  void testSetTermsOfUseAndAccess() {
+
+  }
+
+  @Test
+  void testSetUsageCount() {
+
+  }
+
+  @Test
+  void testUpdateInstructions() {
+
+  }
+}
